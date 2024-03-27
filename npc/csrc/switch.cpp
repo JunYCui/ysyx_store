1cefe9cd87951e89b982daf44b4df39c2f60a6c9
tracer-ysyx <tracer@ysyx.org>
2024-03-27 16:30:59 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:30:59 up 14 min,  1 user,  load average: 0.37, 0.49, 0.32

---
+++ npc/csrc/switch.cpp
@@ -0,0 +1,47 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "assert.h"
+
+#include "Vswitch.h"
+#include <verilated.h>
+
+#include "verilated_vcd_c.h"
+
+//#include "nvboard.h"
+
+//static Vswitch dut;
+
+int main(int argc,char** argv )
+{
+    VerilatedContext *contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vswitch* top = new Vswitch{contextp};
+    
+    // bind all pins
+   // nvboard_bind_all_pins(&dut);
+    //nvboard_init();
+    
+    //wave trace
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    contextp->traceEverOn(true);
+    top->trace(tfp,0);
+    tfp->open("wave.vcd");
+
+    while(!contextp->gotFinish()){    
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+   // nvboard_update();
+   // single_cycle();
+    tfp->dump(contextp->time());
+    contextp->timeInc(1);
+    printf("a = %d, b = %d, f= %d \n ",a,b,top->f);
+    assert(top->f == (a^b));
+    }
+   // nvboard_quit();
+    delete top;
+    tfp->close();
+    delete contextp;
+    return 0;
