cb8bd848927c1d33be1f93cedf351a92403de3c4
tracer-ysyx <tracer@ysyx.org>
2024-03-29 17:02:06 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:02:06 up 24 min,  1 user,  load average: 0.42, 0.33, 0.28

--- npc/csrc/light.cpp
+++ npc/csrc/light.cpp
@@ -2,14 +2,26 @@
 #include "stdlib.h"
 #include "assert.h"
 
-#include "Vswitch.h"
+#include "Vlight.h"
 #include <verilated.h>
-
-#include "verilated_vcd_c.h"
 
 #include "nvboard.h"
 
+
 static TOP_NAME dut;
+
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
+
+
 void nvboard_bind_all_pins(TOP_NAME* top);
 
 int main(int argc,char** argv )
---
+++ npc/vsrc/light.v
@@ -0,0 +1,29 @@
+module light
+(
+    input clk,
+    input rst,
+    output reg [15:0]led 
+);
+    reg [31:0] count;
+    always @(posedge clk) 
+    begin
+        if(rst) begin count <=1'b0; end    
+        else if(count == 32'd5000000)
+                count <= 1'b0;
+        else 
+            begin
+                count <= count + 1'b1;
+            end
+    end
+    always@(posedge clk)
+    begin
+        if(rst) begin led <= 16'd1 end
+        else if(count == 32'd5000000)
+            led <= {led[14:0],led[15]};
+        else 
+            led <= led;
+    end
+
+
+
+endmodule
