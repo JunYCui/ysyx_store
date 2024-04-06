7a001860af303c22ac7327473db66c189bcb8a23
tracer-ysyx <tracer@ysyx.org>
2024-04-06 13:22:48 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:22:48 up  3:31,  1 user,  load average: 0.43, 0.32, 0.30

--- npc/vsrc/translation.v
+++ npc/vsrc/translation.v
@@ -7,14 +7,14 @@ always @(x or en)
 begin
     casex (x)   
     {
-    8'b1xxxxxxx:led = 3'b111; 
-    8'bx1xxxxxx:led = 3'b110;    
-    8'bxx1xxxxx:led = 3'b101;
-    8'bxxx1xxxx:led = 3'b100;
-    8'bxxxx1xxx:led = 3'b011;
-    8'bxxxxx1xx:led = 3'b010;
-    8'bxxxxxx1x:led = 3'b001;
-    8'bxxxxxxx1:led = 3'b000;
+    8'b1xxxxxxx :led = 3'b111; 
+    8'bx1xxxxxx :led = 3'b110;    
+    8'bxx1xxxxx :led = 3'b101;
+    8'bxxx1xxxx :led = 3'b100;
+    8'bxxxx1xxx :led = 3'b011;
+    8'bxxxxx1xx :led = 3'b010;
+    8'bxxxxxx1x :led = 3'b001;
+    8'bxxxxxxx1 :led = 3'b000;
     default: led =3'b000;
     }
     endcase
