b9670c0300826e7f8c7bedd6feddca86afcb74ee
tracer-ysyx <tracer@ysyx.org>
2024-04-06 15:27:11 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:27:11 up  5:19,  1 user,  load average: 1.33, 1.26, 1.04

--- npc/vsrc/seg7.v
+++ npc/vsrc/seg7.v
@@ -5,17 +5,17 @@ output reg [7:0]seg_out;
 /* verilator lint_off WIDTHEXPAND */
 always @(*) begin
     case (num)
-    4'd0:seg_out = 8'h3f;
-    4'd1:seg_out = 8'h06;
-    4'd2:seg_out = 8'h5b;
-    4'd3:seg_out = 8'h4f;
-    4'd4:seg_out = 8'h66;
-    4'd5:seg_out = 8'h6d;
-    4'd6:seg_out = 8'h7d;
-    4'd7:seg_out = 8'h07;
-    4'd8:seg_out = 8'h7f;
-    4'd9:seg_out = 8'h6f;
-    default: seg_out = 8'h00;
+    4'd0:seg_out = 8'b00000000;
+    4'd1:seg_out = 8'b00000000;
+    4'd2:seg_out = 8'b00000000;
+    4'd3:seg_out = 8'b00000000;
+    4'd4:seg_out = 8'b00000000;
+    4'd5:seg_out = 8'b00000000;
+    4'd6:seg_out = 8'b00000000;
+    4'd7:seg_out = 8'b00000000;
+    4'd8:seg_out = 8'b00000000;
+    4'd9:seg_out = 8'b00000000;
+    default: seg_out = 8'b00000000;
     endcase
 end
 /* verilator lint_on WIDTHEXPAND */
