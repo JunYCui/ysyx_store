802418ae52eab2a227b6da6e9c8c0184c617fe13
tracer-ysyx <tracer@ysyx.org>
2024-04-06 15:34:54 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:34:54 up  5:26,  1 user,  load average: 1.45, 1.30, 1.13

--- npc/vsrc/seg7.v
+++ npc/vsrc/seg7.v
@@ -7,14 +7,14 @@ always @(*) begin
     case (num)
     4'd0:seg_out = 8'b00000011;
     4'd1:seg_out = 8'b10011111;
-    4'd2:seg_out = 8'b00000000;
-    4'd3:seg_out = 8'b00000000;
-    4'd4:seg_out = 8'b00000000;
-    4'd5:seg_out = 8'b00000000;
-    4'd6:seg_out = 8'b00000000;
-    4'd7:seg_out = 8'b00000000;
-    4'd8:seg_out = 8'b00000000;
-    4'd9:seg_out = 8'b00000000;
+    4'd2:seg_out = 8'b00100101;
+    4'd3:seg_out = 8'b00001101;
+    4'd4:seg_out = 8'b10011001;
+    4'd5:seg_out = 8'b01001001;
+    4'd6:seg_out = 8'b01000001;
+    4'd7:seg_out = 8'b00011111;
+    4'd8:seg_out = 8'b00000001;
+    4'd9:seg_out = 8'b00001001;
     default: seg_out = 8'b00000000;
     endcase
 end
