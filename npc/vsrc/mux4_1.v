dd2010b51a07fefed9fd5fe2ed597543d030a32d
tracer-ysyx <tracer@ysyx.org>
2024-04-06 10:17:02 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:17:02 up  1:32,  1 user,  load average: 0.13, 0.23, 0.26

--- npc/vsrc/mux4_1.v
+++ npc/vsrc/mux4_1.v
@@ -17,9 +17,10 @@ MuxKey #(4,2,2) inst(
 );
 
 endmodule
-/* verilator lint_off MuxKeyInternal */
+
 module MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) (
-  output reg [DATA_LEN-1:0] out,
+ /* verilator lint_off MuxKeyInternal */ 
+ output reg [DATA_LEN-1:0] out,
   input [KEY_LEN-1:0] key,
   input [DATA_LEN-1:0] default_out,
   input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
@@ -53,8 +54,9 @@ module MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) 
   end
 
 endmodule
-/* verilator lint_off MuxKey */
+
 module MuxKey #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
+/* verilator lint_off MuxKey */
   output [DATA_LEN-1:0] out,
   input [KEY_LEN-1:0] key,
   input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
