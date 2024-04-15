a33ed172b7f831fab9e3f97bf7effc8359f95fa5
tracer-ysyx <tracer@ysyx.org>
2024-04-15 16:59:03 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:03 up  3:30,  1 user,  load average: 0.50, 0.46, 0.34

--- npc/vsrc/ALU.v
+++ npc/vsrc/ALU.v
@@ -11,15 +11,15 @@ module ALU(
     always@(*)
     begin
     case(choice)
-    3'b000:{Carry,res} = d1 + d2;
-           overflow = ((d1[3] == d2[3])&&(d1[3]!= res[3]));
-    3'b001:{Carry,res} = d1 + ~d2 + 1'b1;
+    3'b000: {Carry,res} = d1 + d2;
             overflow = ((d1[3] == d2[3])&&(d1[3]!= res[3]));
-    3'b010:res = ~d1;
+    3'b001: {Carry,res} = d1 + ~d2 + 1'b1;
+            overflow = ((d1[3] == d2[3])&&(d1[3]!= res[3]));
+    3'b010: res = ~d1;
 
-    3'b011:res = d1 & d2;
-    3'b100:res = d1 | d2;
-    3'b101:res = d1 ^ d2;
+    3'b011: res = d1 & d2;
+    3'b100: res = d1 | d2;
+    3'b101: res = d1 ^ d2;
     
     3'b110:if(d1>d2)compare <= 1'b1 else compare <= 1'b0 
     3'b111:if(d1==d2)compare = 1'b1 else compare <= 1'b0
