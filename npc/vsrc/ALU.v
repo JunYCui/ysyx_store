e814f60d2ac3dfcc09fafa8f4f9d80f673b3abb3
tracer-ysyx <tracer@ysyx.org>
2024-04-17 22:03:26 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:03:26 up 33 min,  1 user,  load average: 0.50, 0.34, 0.29

--- npc/vsrc/ALU.v
+++ npc/vsrc/ALU.v
@@ -3,32 +3,31 @@ module ALU(
     input [3:0] d2,
     input [2:0] choice,
     output reg [3:0] res,
-    output reg Carry,
     output reg overflow,
     output reg compare
 );
+    reg [3:0]res_compare; 
     always@(*)
     begin
     case(choice)
     3'b000: begin 
-            {Carry,res} = d1 + d2;
+            res = d1 + d2;
             overflow = ((d1[3] == d2[3])&(d1[3]!= res[3]));
             compare  = 1'b0;
             end
     3'b001: begin
-         /* verilator lint_off WIDTHEXPAND */
-            {Carry,res} = d1 + (~d2) + 1'b1;
-         /* verilator lint_on WIDTHEXPAND */
-            overflow = ((d1[3] == d2[3])&(d1[3]!= res[3]));
-            compare  = 1'b0;
+                /* verilator lint_off WIDTHEXPAND */
+                res  = d1 + (~d2) + 1'b1 ;
+                overflow = ((d1[3] == d2[3])&(d1[3]!= res[3]));
+                 /* verilator lint_on WIDTHEXPAND */
+                compare  = 1'b0;
             end
     3'b010: begin 
         /* verilator lint_off WIDTHEXPAND */
-            res = ~d1;
+            res = ~d1+1'b1;
         /* verilator lint_on WIDTHEXPAND */
             overflow = 1'b0;
-            compare  = 1'b0;
-            Carry = 1'b0;           
+            compare  = 1'b0;      
             end
     3'b011: begin 
             res = d1 & d2;
@@ -49,18 +48,44 @@ module ALU(
             Carry = 1'b0;                 
             end
     3'b110:begin 
-            if(d1>d2)compare  = 1'b1; 
-            else compare  = 1'b0; 
+            res_compare = d1 + (~d2) + 1'b1;
+            if((d1[3] == d2[3])&&(d1[3]!= res_compare[3]))
+            {
+                if(res_compare[3]==1)
+                {
+                  compare = 1'b0; 
+                }
+                else 
+                {
+                  compare = 1'b1; 
+                }
+            }
+            else 
+            {
+                if(res_compare[4]==0)
+                {
+                 compare = 1'b0; 
+                }
+                else 
+                {
+                 compare = 1'b1;
+                }
+            }
             res = 4'b0;
             overflow = 1'b0;
-            Carry = 1'b0; 
             end
     3'b111:begin 
-            if(d1==d2)compare  = 1'b1; 
-            else compare  = 1'b0; 
+            res_compare = d1 + (~d2) + 1'b1;
+            if(res_compare[2:0] == 3'b000)
+            {
+                compare  = 1'b1;
+            }
+            else 
+            {
+                compare = 1'b1;
+            }
             res = 4'b0;
             overflow = 1'b0;
-            Carry = 1'b0; 
             end
     endcase
     
