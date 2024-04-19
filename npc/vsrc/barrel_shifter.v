31f772709ef16a695a89e9221e2b3bbfed734316
tracer-ysyx <tracer@ysyx.org>
2024-04-19 08:59:56 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:59:56 up 57 min,  1 user,  load average: 0.40, 0.47, 0.41

--- npc/vsrc/barrel_shifter.v
+++ npc/vsrc/barrel_shifter.v
@@ -7,8 +7,63 @@ module barrel_shifter
 
     output dout[7:0]
 );
+    reg [7:0]din_1,din_2;
+    always@(*)
+    begin
+        if(shamt[0] == 1'b1)
+       begin
+        if(ari == 1'b1)
+            if(dir == 1'b1)
+                din_1 = {din[6:0],1'b0};
+            else 
+                din_1 = {1'b0,din[7:1]};
+        else
+            if(dir == 1'b1)
+                din_1 = {din[6:0],1'b0};
+            else 
+                din_1 = {din[7],din[7:1]};
+       end
+        else 
+            din_1 = din; 
+    end
 
+    always@(*)
+    begin
+        if(shamt[1] == 1'b1)
+       begin
+        if(ari == 1'b1)
+            if(dir == 1'b1)
+                din_2 = {din_1[5:0],2'b00};
+            else 
+                din_2 = {2'b00,din_1[7:2]};
+        else
+            if(dir == 1'b1)
+                din_2 = {din_1[5:0],2'b00};
+            else 
+                din_2 = {din_1[7],din_2[7],din_1[7:2]};
+       end
+        else 
+            din_1 = din_2; 
+    end
 
+        always@(*)
+    begin
+        if(shamt[1] == 1'b1)
+       begin
+        if(ari == 1'b1)
+            if(dir == 1'b1)
+                dout = {din_2[4:0],3'b000};
+            else 
+                dout = {3'b000,din_2[7:3]};
+        else
+            if(dir == 1'b1)
+                dout = {din_2[4:0],3'b000};
+            else 
+                dout = {din_2[7],din_2[7],din_2[7],din_2[7:3]};
+       end
+        else 
+            dout = din_2; 
+    end
 
 
 
