7febc3e18b960a0ea737cd7cadfa0b53693f60d4
tracer-ysyx <tracer@ysyx.org>
2024-04-19 10:45:53 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:45:53 up  2:29,  1 user,  load average: 0.22, 0.37, 0.39

--- npc/vsrc/LSFR.v
+++ npc/vsrc/LSFR.v
@@ -5,12 +5,13 @@ module LSFR (
     output [7:0]seg1,
     output [7:0]seg2
 );
+
 /* verilator lint_off WIDTHTRUNC */
     reg [7:0]dat;
     reg [31:0]clk_count;
     reg data_move;
     reg clk_fen;
-    wire [3:0] unit,ten,hundred;
+    wire [2:0] unit,ten,hundred;
 
 
     always @(posedge clk) begin
