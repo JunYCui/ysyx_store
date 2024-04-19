ba1c06b9317a4fd165d33c77c55ffc985484073c
tracer-ysyx <tracer@ysyx.org>
2024-04-19 08:29:56 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:29:56 up 27 min,  1 user,  load average: 0.50, 0.42, 0.38

--- npc/vsrc/mux4_1.v
+++ npc/vsrc/mux4_1.v
@@ -1,66 +1,27 @@
-module mux4_1(a,b,c,d,s,led);
+module mux4_1
+(
+    input din0,
+    input din1,
+    input din2,
+    input din3,
+    input s0,
+    input s1,
 
-input [1:0]a,b,c,d; 
-input [1:0]s;
-
-output [1:0]led;
-
-MuxKey #(4,2,2) inst(
-    led,
-    s,
-    {
-    2'b00, a,
-    2'b01, b,
-    2'b10, c,
-    2'b11, d
-    }
+    output out
 );
+    always(*)
+    begin
+        case({s1,s0})
 
-endmodule
-/* verilator lint_off DECLFILENAME */
-module MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) (
-
- output reg [DATA_LEN-1:0] out,
-  input [KEY_LEN-1:0] key,
-  input [DATA_LEN-1:0] default_out,
-  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
-);
-
-  localparam PAIR_LEN = KEY_LEN + DATA_LEN;
-  wire [PAIR_LEN-1:0] pair_list [NR_KEY-1:0];
-  wire [KEY_LEN-1:0] key_list [NR_KEY-1:0];
-  wire [DATA_LEN-1:0] data_list [NR_KEY-1:0];
-
-  generate
-    for (genvar n = 0; n < NR_KEY; n = n + 1) begin
-      assign pair_list[n] = lut[PAIR_LEN*(n+1)-1 : PAIR_LEN*n];
-      assign data_list[n] = pair_list[n][DATA_LEN-1:0];
-      assign key_list[n]  = pair_list[n][PAIR_LEN-1:DATA_LEN];
+        2'b00:out <= din0; 
+        2'b01:out <= din1;
+        2'b10:out <= din2;
+        2'b11:out <= din3;
+        
+        endcase 
     end
-  endgenerate
 
-  reg [DATA_LEN-1 : 0] lut_out;
-  reg hit;
-  integer i;
-  always @(*) begin
-    lut_out = 0;
-    hit = 0;
-    for (i = 0; i < NR_KEY; i = i + 1) begin
-      lut_out = lut_out | ({DATA_LEN{key == key_list[i]}} & data_list[i]);
-      hit = hit | (key == key_list[i]);
-    end
-    if (!HAS_DEFAULT) out = lut_out;
-    else out = (hit ? lut_out : default_out);
-  end
+
+
 
 endmodule
-
-module MuxKey #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
-  output [DATA_LEN-1:0] out,
-  input [KEY_LEN-1:0] key,
-  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
-);
-  MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 0) i0 (out, key, {DATA_LEN{1'b0}}, lut);
-endmodule
-
-/* verilator lint_on DECLFILENAME */
