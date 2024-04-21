959d5c925bb05dad8eda890e6522771165348a19
tracer-ysyx <tracer@ysyx.org>
2024-04-21 18:45:39 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:45:39 up  2:04,  1 user,  load average: 0.39, 0.39, 0.30

---
+++ npc/vsrc/top.v
@@ -0,0 +1,79 @@
+module top
+(
+input ps2_clk,
+input ps2_dat,
+input clk,
+input clrn,
+
+output [15:0] led
+
+);
+
+wire ready;
+wire [7:0]dat;
+wire nextdat_n;
+reg f1_flag;
+reg [2:0]state,next_state;
+
+
+
+
+always @(posedge clk or negedge clrn)
+    if(clrn == 1'b0)
+        nextdat_n <= 1'b1;
+    else if( fi_flag == 1'b1)
+        nextdat_n <= 1'b0;
+    else 
+        nextdat_n <= 1'b1;
+
+always @(posedge clk or negedge clrn)
+    if(clrn == 1'b0)
+        state <= 3'd0;
+    else 
+        state <= next_state;
+
+always @(posedge clk or negedge clrn)
+    if(clrn == 1'b0)
+        next_state <= 3'd0;
+    else  
+        begin
+            case (state)
+            3'd0:if(ready) next_state <= 3'd1 else next_state<=3'b0;
+            3'd1:if(f1_flag == 1'b1) next_state <= 3'd2;
+            3'd2:if(nextdat_n == 1'b0) next_state <= 3'd0; 
+            default: next_state <= 3'd0;
+            endcase    
+        end
+
+always @(posedge clk or negedge clrn)
+    if(clrn == 1'b0)
+        f1_flag <= 1'd0;
+    else if(state == 1'd1)
+        f1_flag <= 1'd1;
+    else 
+        f1_flag <= f1_flag;
+
+always @(posedge clk or negedge clrn)
+    if(clrn == 1'b0)
+        led <= 16'd0;
+    else if (state == 1'd1)
+        led <= {8'd0,dat}
+    else 
+        led <= led;
+
+
+ps2_keyboard ps2_keyboard_inst
+(   
+    .clk(clk),
+    .clrn(clrn),
+    .ps2_clk(ps2_clk),
+    .ps2_data(ps2_dat),
+    .data(dat),
+    .ready(ready),
+    .nextdata_n(nextdat_n),
+    .overflow(overflow)
+);
+
+
+
+endmodule
