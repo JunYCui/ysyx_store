cf68afede635a4fcaea3d0d09c40618f7e8a7983
tracer-ysyx <tracer@ysyx.org>
2024-04-23 14:31:14 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:31:14 up 17 min,  1 user,  load average: 0.77, 1.18, 0.93

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -94,7 +94,7 @@ always @(posedge clk)
 always @(posedge clk)
     if(rst == 1'b0)
         keyvalue <= 8'd0;
-    else if(state == 1'b2)
+    else if(state == 3'd2)
         keyvalue <= dat;
     else if(flag == 1'b1 &&state == 3'd1)
         keyvalue <= 8'h00;
@@ -111,13 +111,13 @@ always@(posedge clk)
 /* verilator lint_off WIDTHTRUNC */
 seg7 seg7_inst1(
     .num(keyvalue%16),
-    .clear(clear)
+    .clear(clear),
     .seg_out(seg1)
 );
 
 seg7 seg7_inst2(
     .num(keyvalue/16),
-    .clear(clear)
+    .clear(clear),
     .seg_out(seg2)
 );
 
