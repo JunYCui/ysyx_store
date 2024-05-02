60ef2ead8600df37b3c430525743aa92f6e7c13a
tracer-ysyx <tracer@ysyx.org>
2024-05-02 09:39:35 +0800

    > compile NEMU ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:39:35 up  1:33,  1 user,  load average: 0.53, 0.58, 0.53

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -154,13 +154,13 @@ static uint8_t check_parentheses(uint8_t p, uint8_t q)
         {
           state--;
         }
-
-        if(state<0&&state>-2)
-        {        
           if(strcmp(tokens[1].str,"35")==0)
         {
           printf("state : %d \n",state);
         }
+        if(state<0&&state>-2)
+        {        
+
           flag = 1;
         }
         else if(state <-1)
