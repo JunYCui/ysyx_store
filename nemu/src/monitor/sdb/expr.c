07c5f9685717d21e16108cca68044a2e31ca2ca8
tracer-ysyx <tracer@ysyx.org>
2024-05-03 08:12:39 +0800

    > compile NEMU ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:12:39 up  6:28,  1 user,  load average: 0.64, 0.50, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -302,6 +302,7 @@ static word_t eval(uint32_t p ,uint32_t q)
               count_N++;
             }
             count++;
+            assert(0);
           }
           return ((-1)^count_N)*atoi(tokens[position+1].str);
         }
