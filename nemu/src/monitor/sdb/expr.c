e020b2b505fa0eab9831924c87218383314199f6
tracer-ysyx <tracer@ysyx.org>
2024-05-03 08:41:32 +0800

    > compile NEMU ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:41:32 up  6:57,  1 user,  load average: 0.18, 0.25, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -301,8 +301,9 @@ static word_t eval(uint32_t p ,uint32_t q)
     {
       if(tokens[position].type == TK_NEG)
       {
+          printf("%d \n",position);
           val1 = - eval(position+1,p);
-          printf("%d",position);
+          
           return val1;
       }
     }
