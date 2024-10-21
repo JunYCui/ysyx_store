8ca05d0d4e13eaedc55d872f774a127b1c932ddc
tracer-ysyx <tracer@ysyx.org>
2024-10-21 16:22:37 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:22:37 up 18:51,  4 users,  load average: 0.32, 0.52, 0.45

--- npc/csrc/LSFR.cpp
+++ npc/csrc/LSFR.cpp
@@ -14,10 +14,9 @@ static void single_cycle() {
 int main() {
   nvboard_bind_all_pins(&dut);
   nvboard_init();
-  
-
   while(1) {
     nvboard_update();
     single_cycle();
   }
+    nvboard_quit();
 }
