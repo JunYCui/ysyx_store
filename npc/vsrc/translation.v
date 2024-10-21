8d3ec034716278cc4dd05a3ca1220128f060d9ac
tracer-ysyx <tracer@ysyx.org>
2024-10-21 21:49:19 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:19 up 20:38,  4 users,  load average: 0.42, 0.55, 0.54

--- npc/vsrc/translation.v
+++ npc/vsrc/translation.v
@@ -10,23 +10,23 @@ always @(*)
 begin
     if(en)
     begin
-    signal <= 1'b1;
+    signal = 1'b1;
     casex (x) 
-    8'b1xxxxxxx :led <= 3'b111; 
-    8'b01xxxxxx :led <= 3'b110;    
-    8'b001xxxxx :led <= 3'b101;
-    8'b0001xxxx :led <= 3'b100;
-    8'b00001xxx :led <= 3'b011;
-    8'b000001xx :led <= 3'b010;
-    8'b0000001x :led <= 3'b001;
-    8'b00000001 :led <= 3'b000;
-    default: led <=3'b000;
+    8'b1xxxxxxx :led = 3'b111; 
+    8'b01xxxxxx :led = 3'b110;    
+    8'b001xxxxx :led = 3'b101;
+    8'b0001xxxx :led = 3'b100;
+    8'b00001xxx :led = 3'b011;
+    8'b000001xx :led = 3'b010;
+    8'b0000001x :led = 3'b001;
+    8'b00000001 :led = 3'b000;
+    default: led =3'b000;
     endcase 
    end
    else 
    begin
-   led <= 3'b000;
-   signal <= 1'b1;
+   led = 3'b000;
+   signal = 1'b1;
    end
 end
 /* verilator lint_on CASEX */
