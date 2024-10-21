2d9ea14c620a118da3543f53ab365a2ce8fd5bb2
tracer-ysyx <tracer@ysyx.org>
2024-10-21 21:20:10 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:20:10 up 20:09,  4 users,  load average: 1.57, 0.90, 0.58

--- npc/vsrc/mux4_1.v
+++ npc/vsrc/mux4_1.v
@@ -4,14 +4,13 @@ module mux4_1
     input [1:0]din1,
     input [1:0]din2,
     input [1:0]din3,
-    input s0,
-    input s1,
+    input [1:0]s0,
 
     output reg [1:0]out
 );
     always@(*)
     begin
-        case({s1,s0})
+        case(s0)
         2'b00:out = din0; 
         2'b01:out = din1;
         2'b10:out = din2;
