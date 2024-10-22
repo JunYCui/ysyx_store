cb3831734f2cea6b69e21430526ef3fd0b769419
tracer-ysyx <tracer@ysyx.org>
2024-10-22 15:43:52 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:52 up 1 day,  1:49,  4 users,  load average: 1.13, 0.59, 0.50

--- npc/vsrc/add.v
+++ npc/vsrc/add.v
@@ -17,7 +17,7 @@ assign add_3 = (choose_add_sub == 1'b1)? add_2:(~add_2 + 1'b1);
 
 assign result = add_1 + add_3;
 
-assign overflow = (add_1[BW-1] == add_2[BW-1])? ( (result[BW-1] == add_1[BW-1])? 1'b0:1'b1):(1'b0) ;
+assign overflow = (add_1[BW-1] == add_3[BW-1])? ( (result[BW-1] == add_1[BW-1])? 1'b0:1'b1):(1'b0) ;
 
 
 
