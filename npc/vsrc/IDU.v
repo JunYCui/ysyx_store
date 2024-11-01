c66c4187bc41bc639bd52ae973df16ede89665c4
tracer-ysyx <tracer@ysyx.org>
2024-11-01 22:48:43 +0800

    > sim RTL ysyx_22040000 Cui Jun Yan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:48:43 up  2:37,  1 user,  load average: 0.60, 0.48, 0.40

--- npc/vsrc/IDU.v
+++ npc/vsrc/IDU.v
@@ -1,25 +1,29 @@
 `include "vsrc/para.v"
-/* verilator lint_off UNUSEDPARAM */
+
 localparam i0_NR_KEY  = 4'd9;
 localparam i0_KEY_LEN = 4'd7;
 localparam i0_DATA_LEN= 4'd4;
 
 module IDU(
-    input [31:0]PC  ,
-    output[4 :0]rs1 ,
-    output[4 :0]rs2 ,
-    output[4 :0]rd  ,
-    output[20:0]imm ,
-    output[3: 0]Type,
+    input [31:0]PC      ,
+    output[4 :0]rs1     ,
+    output[4 :0]rs2     ,
+    output[4 :0]rd      ,
+    output[20:0]imm     ,
+    output[14:12]funct3 ,
+    output[3: 0]Type    ,
     output[6:0]opcode
 );
 
 
-
-assign opcode = PC[6:0];
+assign key    = PC[6:0]  ;
+assign opcode = PC[6:0]  ;    
+assign rs1  =   PC[19:15];
+assign rs2  =   PC[24:20];
+assign funct3 = PC[14:12];
 
 // choose type
-MuxKeyInternal #(i0_NR_KEY, i0_KEY_LEN, i0_DATA_LEN, 0) i0 (out, key, {i0_DATA_LEN{1'b0}}, 
+MuxKeyInternal #(i0_NR_KEY, i0_KEY_LEN, i0_DATA_LEN, 0) i0 (Type, opcode, {i0_DATA_LEN{1'b0}}, 
 {R_opcode,R_Type,
  B_opcode,B_Type,
  S_opcode,S_Type,
