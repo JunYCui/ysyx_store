a1e1beec669fd565db107cd0e38815986854466e
tracer-ysyx <tracer@ysyx.org>
2024-11-10 14:34:06 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:34:06 up  1:28,  1 user,  load average: 0.14, 0.17, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -120,11 +120,11 @@ void assert_fail_msg() {
     {
       if(i == flag)
       {
-        printf("   \t%s\n",trace_fifo[i].logbuf);
+        printf("-->  \t%s\n",trace_fifo[i].logbuf);
       }
       else 
       {
-        printf("-->\t%s\n",trace_fifo[i].logbuf);
+        printf("\t%s\n",trace_fifo[i].logbuf);
       }
     }
   }
