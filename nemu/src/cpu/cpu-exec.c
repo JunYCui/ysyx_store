f9beec3ba206f73748f8ab5e277827c6501b7478
tracer-ysyx <tracer@ysyx.org>
2024-11-10 10:14:37 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:14:37 up 6 min,  1 user,  load average: 0.53, 0.58, 0.28

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -98,12 +98,18 @@ static void execute(uint64_t n) {
 }
 
 static void statistic() {
+  uint8_t i=0;
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
 #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
   if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
+
+  for(i=0;i<64;i++)
+  {
+    puts(trace_fifo[i].logbuf);
+  }
 }
 
 void assert_fail_msg() {
