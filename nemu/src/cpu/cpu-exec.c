7e6211358df8967d0001587109d0d8dd4da53ce4
tracer-ysyx <tracer@ysyx.org>
2024-11-10 10:10:59 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:10:59 up 2 min,  1 user,  load average: 1.21, 0.48, 0.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -25,16 +25,22 @@ void Cpu_Wp(void);
  * You can modify this value as you want.
  */
 #define MAX_INST_TO_PRINT 10
+#define MAX_FIFO_BUF 64
 
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 
-
+struct Ring
+{
+  char logbuf[128];
+}trace_fifo[MAX_FIFO_BUF];
+static uint8_t fifo_count=0;
 
 
 void device_update();
+
 
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) 
@@ -43,7 +49,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
-  if (g_print_step) { ;}//IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); strcpy(trace_fifo[fifo_count++].logbuf,_this->logbuf);if(fifo_count == MAX_FIFO_BUF){fifo_count = 0;} }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   if (CONFIG_WATCHPOINT) Cpu_Wp();
 
