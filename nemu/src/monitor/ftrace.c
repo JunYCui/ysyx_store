16823f5dfe3dfc1af508b06369ec0c68cb5fa4a3
tracer-ysyx <tracer@ysyx.org>
2024-11-11 23:53:41 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:53:41 up  3:40,  1 user,  load average: 0.68, 0.40, 0.33

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -22,7 +22,7 @@ static void find_symtab_32(FILE* fp)
     fseek(fp,section_off,SEEK_SET);
     num = fread(Eshdr,sizeof(Elf32_Shdr),section_num,fp);
     assert(num == section_num);
-    printf("[Nr]\t Name \t\t Type \t Addr \t Off \t Size \t ES \t Flg \t Lk \t Inf \t Al \t \n");
+    printf("[Nr]\t Name \t Type \t\t Addr \t Off \t Size \t ES \t Flg \t Lk \t Inf \t Al \t \n");
     for(int i=0;i<section_num;i++)
     {
     switch (Eshdr[i].sh_type)
@@ -46,7 +46,7 @@ static void find_symtab_32(FILE* fp)
     default:strcpy(type,"error");
         break;
     }
-    printf("[%d]\t %u \t\t %s \t Addr \t Off \t Size \t ES \t Flg \t Lk \t Inf \t Al \t \n",i,Eshdr[i].sh_name,type);
+    printf("[%d]\t %u \t %s \t\t Addr \t Off \t Size \t ES \t Flg \t Lk \t Inf \t Al \t \n",i,Eshdr[i].sh_name,type);
     
     }
     free(Ehdr);
