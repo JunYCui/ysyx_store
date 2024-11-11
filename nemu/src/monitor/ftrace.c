61c514bdb76901cd952315e6b5ce2843400d2a7d
tracer-ysyx <tracer@ysyx.org>
2024-11-11 20:45:18 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:18 up  1:34,  1 user,  load average: 0.42, 0.34, 0.32

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -3,6 +3,6 @@
 void init_ftrace(char* elf_file)
 {
     Elf32_Ehdr *elf_hdr = (Elf32_Ehdr*)elf_file;
-    printf(" \n ************%d************* \n",elf_hdr->e_ident[3]);
+    printf(" \n ************%c************* \n",elf_hdr->e_ident[3]);
 
 }
