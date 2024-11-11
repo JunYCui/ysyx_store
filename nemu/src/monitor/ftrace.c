d0d2ac27a3235254f0498bdd61685bca9d9d14a3
tracer-ysyx <tracer@ysyx.org>
2024-11-11 23:49:08 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:49:08 up  3:35,  1 user,  load average: 0.31, 0.35, 0.30

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -6,17 +6,51 @@ static void find_symtab_32(FILE* fp)
 {
     Elf32_Ehdr* Ehdr= malloc(sizeof(Elf32_Ehdr)); 
     Elf32_Off section_off;
+    Elf32_Half section_num;
+    char type[20];
     size_t num;
     fseek(fp,0,SEEK_SET);
     num = fread(Ehdr,sizeof(Elf32_Ehdr),1,fp);
     assert(num == 1);
     section_off = Ehdr->e_shoff;
+    section_num = Ehdr->e_shnum;
     if(section_off == 0)
     {
         printf("there is no section header table! \n");
     }
-    printf("%u",section_off);
+    Elf32_Shdr *Eshdr = malloc(sizeof(Elf32_Shdr[section_num]));
+    num = fread(Eshdr,sizeof(Elf32_Shdr),section_num,fp);
+    assert(num == section_num);
+    printf("[Nr]\t Name \t Type \t Addr \t Off \t Size \t ES \t Flg \t Lk \t Inf \t Al \t");
+    for(int i=0;i<section_num;i++)
+    {
+    switch (Eshdr[i].sh_type)
+    {
+    case 0:strcpy(type,"SHT_NULL");break;
+    case 1:strcpy(type,"SHT_PROGBITS");break;
+    case 2:strcpy(type,"SHT_SYMTAB");break;
+    case 3:strcpy(type,"SHT_STRTAB");break;
+    case 4:strcpy(type,"SHT_RELA");break;
+    case 5:strcpy(type,"SHT_HASH");break;
+    case 6:strcpy(type,"SHT_DYNAMIC");break;
+    case 7:strcpy(type,"SHT_NOTE");break;
+    case 8:strcpy(type,"SHT_NOBITS");break;
+    case 9:strcpy(type,"SHT_REL");break;
+    case 10:strcpy(type,"SHT_SHLIB");break;
+    case 11:strcpy(type,"SHT_DYNSYM");break;
+    case 0x70000000:strcpy(type,"SHT_LOPROC");break;
+    case 0x7fffffff:strcpy(type,"SHT_HIPROC");break;
+    case 0x80000000:strcpy(type,"SHT_LOUSER");break;
+    case 0xffffffff:strcpy(type,"SHT_HIUSER");break;
+    default:
+        break;
+    }
+    printf("[%d]\t %d \t %s \t Addr \t Off \t Size \t ES \t Flg \t Lk \t Inf \t Al \t",i,Eshdr[i].sh_name,type);
+    
+    }
+    fseek(fp,section_off,SEEK_SET);
     free(Ehdr);
+    free(Eshdr);
 }
 
 
