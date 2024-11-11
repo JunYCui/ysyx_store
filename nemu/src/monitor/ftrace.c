28682bb3e0eff69e78ac595dc0791d80ffd86da7
tracer-ysyx <tracer@ysyx.org>
2024-11-11 22:49:17 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:49:17 up  2:35,  1 user,  load average: 0.07, 0.13, 0.22

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -4,8 +4,27 @@ void init_ftrace(char* elf_file)
 {
     char str[20];
     size_t num;
-    FILE* fp= fopen(elf_file,"rb");
-    num = fread(str,1,5,fp);
-    if(num == 5)
-    printf("*****%s*****",str);
+    char osType;
+    FILE* fp= fopen(elf_file,"rb");// 读取二进制elf_file的二进制的文件
+    num = fread(str,1,5,fp);// 读取前5个字节到字符串str中
+    if(num != 5)
+    {
+        printf("fread error!\n");
+        assert(0);
+    }
+    if(str[0]!=ELFMAG0 || str[1]!= ELFMAG1 || str[2]!= ELFMAG2 || str[3] !=ELFMAG3 || str[4] == ELFCLASSNONE)
+    {
+        printf("file is not elf-file\n");
+        assert(0);
+    }
+    if(str[4] == ELFCLASS32)
+    {
+        osType = 32;
+    }
+    else 
+    {
+        osType = 64;
+    }
+
+    printf("********%d**********",osType);
 }
