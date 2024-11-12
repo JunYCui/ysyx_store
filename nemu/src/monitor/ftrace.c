afb5d79666a46fa9227433665b2fcc9aa61cbf70
tracer-ysyx <tracer@ysyx.org>
2024-11-12 23:09:14 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:09:14 up  7:28,  1 user,  load average: 0.52, 0.47, 0.50

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -229,10 +229,6 @@ void ftrace_exe(Decode* s)
                    {    
                     ftnum--;
                     printf("0x%x",s->pc);
-                     for(int j=0;j<ftnum;j++)
-                    {
-                    printf(" ");
-                    }
                     printf("rt\n");
                     break;
                    }
@@ -252,12 +248,7 @@ void ftrace_exe(Decode* s)
                    if(func_array[i].addr == s->dnpc) 
                    {
                     printf("0x%x:",s->pc);
-                    for(int j=0;j<ftnum;j++)
-                    {
-                    printf(" ");
-                    }
                     printf("call [%s@0x%x]\n",func_array[i].name,func_array[i].addr);
-                    ftnum++;
                     break;
                    }
                 }
