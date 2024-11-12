b04b6da9332216945f28ba195eed3fe99d9372ed
tracer-ysyx <tracer@ysyx.org>
2024-11-12 23:25:46 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:25:46 up  7:45,  1 user,  load average: 0.30, 0.18, 0.25

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -213,6 +213,7 @@ void ftrace_exe(Decode* s)
         }
         else if(strcmp(inst, "jalr") == 0)
         {
+            printf("%s, %s \n",rd,rs1);
              if(strcmp(rd,"zero")==0 && strcmp(rs1,"0(ra)") ==0)
              {
                 for(int i=0;i<FUNC_MAXNUM;i++)
