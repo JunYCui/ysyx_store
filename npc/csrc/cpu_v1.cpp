d167595caf901c8803e5061f6288096721ca687c
tracer-ysyx <tracer@ysyx.org>
2024-11-14 23:01:44 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:01:44 up  2:57,  1 user,  load average: 0.40, 0.46, 0.44

--- npc/csrc/cpu_v1.cpp
+++ npc/csrc/cpu_v1.cpp
@@ -64,7 +64,7 @@ int main(int argc,char* argv[])
     VerilatedVcdC *m_trace = new VerilatedVcdC;
     // 将 m_trace 与 top 进行关联，其中5表示波形的采样深度为5级以下
 
-
+  printf(" %ld ",sizeof(img) );
     top->trace(m_trace, 5);
     m_trace->open("waveform.vcd");
     top->clk = 0;
@@ -80,9 +80,7 @@ int main(int argc,char* argv[])
     top->clk ^=1;
     if(top->clk == 0)
     {
-    top->inst = pmem_read(top->pc);
-    printf(" %ld ",sizeof(img) );
-    
+    top->inst = pmem_read(top->pc);  
     
     }
     top->eval();
