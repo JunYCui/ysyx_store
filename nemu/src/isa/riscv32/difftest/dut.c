991588b134ed7fae18b4fc9d9148850df90c5195
tracer-ysyx <tracer@ysyx.org>
2024-11-14 14:51:45 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:45 up  3:19,  1 user,  load average: 0.50, 0.40, 0.39

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,11 +21,14 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0;i<MUXDEF(CONFIG_RVE, 16, 32);i++)
   {
     if(ref_r->gpr[i] != gpr(i))
+    {
+      printf("0x%x: %s value is error",pc,reg_name(i));
       return false;
+    }
   }
-  if(ref_r->pc != pc)
+  if(ref_r->pc != cpu.pc)
   {
-    printf("%x\n",pc);
+    printf("0x%x: %d value is error",pc, cpu.pc);
     return false;
   }
   return true;
