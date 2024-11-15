ee834a780536c7da2b9f7b030ff9dc7a7f81ee4d
tracer-ysyx <tracer@ysyx.org>
2024-11-15 21:39:51 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:39:51 up  2:13,  1 user,  load average: 0.14, 0.20, 0.29

--- npc/csrc/cpu_v1.cpp
+++ npc/csrc/cpu_v1.cpp
@@ -8,7 +8,7 @@
 
 #include "svdpi.h"
 #include "Vcpu_v1__Dpi.h"
-
+#include "getopt.h"
 
 #define CONFIG_MSIZE 0x8000000 
 #define CONFIG_MBASE 0x80000000
@@ -45,7 +45,43 @@ static uint32_t pmem_read(uint32_t addr) {
   return ret;
 }
 
+static long load_img() {
+  if (img_file == NULL) {
+    printf("No image is given. Use the default build-in image. \n");
+    return 4096; // built-in image size
+  }
 
+  FILE *fp = fopen(img_file, "rb");
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  printf("The image is %s, size = %ld \n", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return size;
+}
+
+static int parse_args(int argc, char *argv[]) {
+  const struct option table[] = {
+    {0          , 0                , NULL,  0 },
+  };
+  int o;
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
+    switch (o) {
+      case 1: img_file = optarg; return 0;
+      default:
+        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+        printf("\n");
+        exit(0);
+    }
+  }
+  return 0;
+}
 // contextp用来保存仿真的时间
 VerilatedContext *contextp = new VerilatedContext;
 
@@ -63,7 +99,11 @@ int main(int argc,char* argv[])
     // 实例化一个 VerilatedVcdC 类型的对象 m_trace，用于波形跟踪
     VerilatedVcdC *m_trace = new VerilatedVcdC;
     // 将 m_trace 与 top 进行关联，其中5表示波形的采样深度为5级以下
-
+    parse_args(argc,argv);
+    
+    long size = load_img();
+    
+    printf("%ld \n",size);
 
     top->trace(m_trace, 5);
     m_trace->open("waveform.vcd");
@@ -81,7 +121,7 @@ int main(int argc,char* argv[])
     if(top->clk == 0)
     {
     top->inst = pmem_read(top->pc);  
-    printf(" %ld \n",top->inst);    
+    printf(" %d \n",top->inst);    
     }
     top->eval();
     //将所有跟踪的信号值写入波形转储文件
