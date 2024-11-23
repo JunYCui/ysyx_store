bae187df0dcd89724b2d0699572024fd5cfcc9b8
tracer-ysyx <tracer@ysyx.org>
2024-11-23 13:00:20 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:00:20 up  1:59,  1 user,  load average: 1.41, 1.00, 0.62

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -2,7 +2,7 @@
 #include "cpu/decode.h"
 
 FUNC_TR func_array[FUNC_MAXNUM];
-void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 
 static void find_strsymtab_32(FILE* fp)
 {
