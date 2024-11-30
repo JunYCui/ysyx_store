cff1c7fca262eec06ba4a9ec715a10c53c6a46e5
tracer-ysyx <tracer@ysyx.org>
2024-11-30 10:13:23 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:13:23 up  8:22,  2 users,  load average: 0.36, 0.69, 0.49

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -121,7 +121,8 @@ int sprintf(char *out, const char *fmt, ...) {
               }
               p1++;
               break;
-
+            case '0':case '1':case '2':case'3':case'4':case'5':
+            case'6': case'7': case'8': case '9': 
             default:
                   panic("parameter is error");
                   assert(0);  
