fc0becd1e23ceefcf6b3db6da5d06320bf9507ba
tracer-ysyx <tracer@ysyx.org>
2024-12-01 16:12:52 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:52 up  3:35,  1 user,  load average: 0.71, 0.51, 0.48

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -16,7 +16,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = read_time() - boot_time;
+  uptime->us = (read_time() - boot_time)/1000;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
