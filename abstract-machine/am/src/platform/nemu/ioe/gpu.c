18fcae9b49098582701cecf7eff96af487019d26
tracer-ysyx <tracer@ysyx.org>
2024-12-02 21:26:23 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:23 up 13:26,  1 user,  load average: 0.49, 0.29, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -8,12 +8,6 @@
 #define screen_height 300
 
 void __am_gpu_init() {
-  int i;
-  int w = 400;  // TODO: get the correct width
-  int h = 300;  // TODO: get the correct height
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i/2;
-  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -34,10 +28,10 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl){
   int w=ctl->w;
   int h=ctl->h;
   int k=0;
-  if (ctl->sync) {
     for(int i=y;i<y+h;i++)
       for(int j=x;i<x+w;j++)
         fb[i*screen_weight+j] = pix[k++];
+  if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
 }
