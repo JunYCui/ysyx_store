e8ab004918408969e446f9ae6ce4f5cfe40b091d
tracer-ysyx <tracer@ysyx.org>
2024-12-07 13:13:07 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:13:07 up  9:06,  1 user,  load average: 0.30, 0.70, 0.68

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -33,7 +33,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   uint8_t *audio_data = (ctl->buf).start;
   uint32_t sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
   uint32_t len = (ctl->buf).end - (ctl->buf).start;
-  uint32_t *fb = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
+  uint8_t *fb = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;
   for(int i=0;i<len;i++)
   {
     fb[j] = audio_data[i];
