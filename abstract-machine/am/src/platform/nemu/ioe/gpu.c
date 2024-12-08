78c614848e3cc0c0d1690eebaaa25c5b775ca877
tracer-ysyx <tracer@ysyx.org>
2024-12-08 21:23:01 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:01 up  5:51,  1 user,  load average: 0.31, 0.34, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -34,7 +34,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl){
     for (int i = y; i < y+h; i++) {
       for (int j = x; j < x+w; j++) {
         fb[w*i+j] = pix[k++]; 
-    printf("w*i+j = %d \n",w*i+j);
+        printf("w*i+j = %d \n",j);
     }
   }
   }
