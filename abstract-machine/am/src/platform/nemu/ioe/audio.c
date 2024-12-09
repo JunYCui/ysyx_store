8708eed9ce9267587a6cb91d48fce45cfddc9cc9
tracer-ysyx <tracer@ysyx.org>
2024-12-09 08:55:04 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:55:04 up  6:54,  1 user,  load average: 1.89, 0.75, 0.33

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -18,10 +18,7 @@ void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-  outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
-  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-  outl(AUDIO_INIT_ADDR, 1);    
+;    
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
@@ -29,15 +26,6 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  int j=0;
-  uint32_t *audio_data = (ctl->buf).start;
-  uint32_t sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
-  uint32_t len = (ctl->buf).end - (ctl->buf).start;
-  uint32_t *fb = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
-  for(int i=0;i<len;i++)
-  {
-    fb[j] = audio_data[i];
-    j = (j+1)%sbuf_size;
-  }
-   outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR)+len);  
+  return ;
+ 
 }
