e496e70630ea6b793677e8d81b56d29963bf03ca
tracer-ysyx <tracer@ysyx.org>
2024-12-09 08:53:47 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:53:47 up  6:53,  1 user,  load average: 0.45, 0.29, 0.16

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -19,21 +19,25 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl){
-
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  if (!ctl->sync && (w == 0 || h == 0)) return;
-  uint32_t *pixels = ctl->pixels;
+   int x = ctl->x;
+   int y = ctl->y;
+   int w = ctl->w; 
+   int h = ctl->h;
+  int k=0;
+  uint32_t *pix = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
-  for (int i = y; i < y+h; i++) {
-    for (int j = x; j < x+w; j++) {
-      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+  uint32_t wh = inl(VGACTL_ADDR);
+  uint16_t sw = wh>>16;
+    for (int i = y; i < y+h; i++) {
+      for (int j = x; j < x+w; j++) {
+        fb[sw*i+j] = pix[k++]; 
     }
   }
-  if (ctl->sync) {
+    if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
 }
+
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
 }
