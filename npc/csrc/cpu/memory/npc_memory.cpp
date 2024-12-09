e0d522a247abef66d37fd7e969cfb812fec9914b
tracer-ysyx <tracer@ysyx.org>
2024-12-09 16:11:47 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:11:47 up 10:58,  1 user,  load average: 0.61, 0.41, 0.52

--- npc/csrc/cpu/memory/npc_memory.cpp
+++ npc/csrc/cpu/memory/npc_memory.cpp
@@ -73,6 +73,7 @@ extern "C" void npc_pmem_write(int addr, int wdata, char wmask)
   }
   else if(addr >=FB_ADDR && addr <FB_ADDR + screen_size())
   {
+    printf("addr = 0x%x \n", addr - FB_ADDR);
     vmem[addr-FB_ADDR] = wdata;
     return;
   }
