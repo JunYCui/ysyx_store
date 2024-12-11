3be90df4a97b0ecedf9342dc7f2bcf69de474cb8
tracer-ysyx <tracer@ysyx.org>
2024-12-11 21:22:44 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:44 up  1:07,  1 user,  load average: 0.41, 0.71, 0.53

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -9,8 +9,8 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
+  uintptr_t  gpr[NR_REGS],mcause,mstatus, mepc;
   void *pdir;
-  uintptr_t  gpr[NR_REGS],mcause, mepc, mstatus;
 };
 
 #ifdef __riscv_e
