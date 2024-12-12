1fd16aad0707d8d3ebb66ab7d59f4cf71a31abde
tracer-ysyx <tracer@ysyx.org>
2024-12-12 21:21:13 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:21:13 up  1:15,  1 user,  load average: 0.43, 0.35, 0.29

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -32,7 +32,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context* cp = (Context *)kstack.end;
-  cp =(Context *)malloc(sizeof(Context));
+  cp =cp + 1;
 
   return cp;
 }
