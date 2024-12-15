a06922b26127b2752691f5f850c7adfb4e903c97
tracer-ysyx <tracer@ysyx.org>
2024-12-15 21:31:52 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:52 up 11:20,  1 user,  load average: 0.83, 0.59, 0.56

--- npc/csrc/cpu/difftest/npc_dut.cpp
+++ npc/csrc/cpu/difftest/npc_dut.cpp
@@ -126,7 +126,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     is_skip_ref = false;
     return;
   }
-
+  printf("ref.pc = 0x%x \n",ref_r.pc);
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
