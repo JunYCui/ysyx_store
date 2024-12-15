be3108d9dbd47228405a186385f9caee2690c875
tracer-ysyx <tracer@ysyx.org>
2024-12-15 20:46:42 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:46:42 up 10:35,  1 user,  load average: 1.60, 1.27, 1.04

--- npc/csrc/cpu/monitor/npc_ftrace.cpp
+++ npc/csrc/cpu/monitor/npc_ftrace.cpp
@@ -188,7 +188,7 @@ void ftrace_exe(Decode* s)
     char* rd;
     char* rs1;
     int ilen = s->snpc - s->pc;//指令长度  
-    assert(0);
+    
     disassemble(str,sizeof(str),s->pc,(uint8_t *)&s->inst, ilen);
     inst = strtok(str,"\t");
     if(inst!= NULL)
