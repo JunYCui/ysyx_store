825047e89a517eb1a681d615c09083c923936bf0
tracer-ysyx <tracer@ysyx.org>
2024-12-15 21:12:26 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:26 up 11:01,  1 user,  load average: 0.45, 0.38, 0.47

--- npc/csrc/cpu/difftest/npc_dut.cpp
+++ npc/csrc/cpu/difftest/npc_dut.cpp
@@ -120,6 +120,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
 
   if (is_skip_ref) {
+    assert(0);
     // to skip the checking of an instruction, just copy the reg state to reference design
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
