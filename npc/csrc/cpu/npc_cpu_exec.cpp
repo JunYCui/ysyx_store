ec5c7c4fb20b7d1b8fc315e88dd5bac100257add
tracer-ysyx <tracer@ysyx.org>
2024-12-15 21:23:28 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:28 up 11:12,  1 user,  load average: 1.18, 0.75, 0.63

--- npc/csrc/cpu/npc_cpu_exec.cpp
+++ npc/csrc/cpu/npc_cpu_exec.cpp
@@ -84,13 +84,14 @@ void cpu_exec(uint32_t n)
     svSetScope(svGetScopeFromName("TOP.cpu_ysyx_24100029"));
         GetInst(&s.inst);    
         exec_once();
+        cpu.pc = top->pc;
+        printf("cpu.pc = 0x%x \n",cpu.pc);
     svSetScope(svGetScopeFromName("TOP.cpu_ysyx_24100029.Reg_Stack_inst0.Reg_inst"));
     for(int j=0;j<32;j++)
     {
         ReadReg(j,&cpu.gpr[j]);
     }
         trace_and_difftest(&s);
-        cpu.pc = top->pc;
         if(npc_state.state !=NPC_RUNNING)
             break;
     }
