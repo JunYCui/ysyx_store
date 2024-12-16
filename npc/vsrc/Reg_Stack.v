2d3ec0d4a63b53d9fc4fba82db73f2ff7ef9bcf7
tracer-ysyx <tracer@ysyx.org>
2024-12-16 21:05:04 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:05:04 up  5:39,  1 user,  load average: 0.41, 0.53, 0.43

--- npc/vsrc/Reg_Stack.v
+++ npc/vsrc/Reg_Stack.v
@@ -1,5 +1,5 @@
 module Reg_Stack(
-    input                        rst                        ,
+    input                        rst_n                      ,
     input                        clk                        ,
     input              [   4: 0] rs1                        ,
     input              [   4: 0] rs2                        ,
@@ -7,23 +7,66 @@ module Reg_Stack(
     input              [  31: 0] rd_value                   ,
 
     input                        R_wen                      ,
+    input              [   3: 0] csr_wen                    ,
+
+    input              [  31: 0] mepc_in                    ,
+    input              [  31: 0] mcause_in                  ,
+    input              [  31: 0] mstatus_in                 ,
+    input              [  31: 0] mtvec_in                   ,
+
 
     output             [  31: 0] rs1_value                  ,
     output             [  31: 0] rs2_value                  ,
-    output             [  31: 0] a0_value                                  
+    output             [  31: 0] a0_value                   ,
+    output             [  31: 0] csr_value                  ,
+    output             [  31: 0] mepc_out                   ,
+    output             [  31: 0] mtvec_out                   
 );
 
     wire               [  31: 0] wdata                      ;
 
+    wire               [  31: 0] mcause_out                 ;
+    wire               [  31: 0] mstatus_out                ;
+
+
 
     assign                       wdata                     = (rd == 5'd0)? 32'd0:rd_value;
+
+    assign                       csr_value                 = (csr_wen[0])? mepc_out   :
+                                                             (csr_wen[1])? mcause_out :
+                                                             (csr_wen[2])? mstatus_out:
+                                                             (csr_wen[3])? mtvec_out:32'd0;
+
+
+
+CSR #(32,0) CSR_inst(
+    .clk                         (clk                       ),
+    .rst_n                       (rst_n                     ),
+    .mepc_in                     (mepc_in                   ),
+    .mcause_in                   (mcause_in                 ),
+    .mstatus_in                  (mstatus_in                ),
+    .mtvec_in                    (mtvec_in                  ),
+    .csr_wen                     (csr_wen                   ),
+
+    .mepc_out                    (mepc_out                  ),
+    .mcause_out                  (mcause_out                ),
+    .mstatus_out                 (mstatus_out               ),
+    .mtvec_out                   (mtvec_out                 ) 
+
+
+);
+
+
+
+
+
 
 RegisterFile #(5, 32) Reg_inst(
     .clk                         (clk                       ),
     .wdata                       (wdata                     ),
     .waddr                       (rd                        ),
     .wen                         (R_wen                     ),
-    .rst                         (rst                       ),
+    .rst_n                       (rst_n                     ),
     .rs1_addr                    (rs1                       ),
     .rs2_addr                    (rs2                       ),
 
--- npc/vsrc/cpu_ysyx_24100029.v
+++ npc/vsrc/cpu_ysyx_24100029.v
@@ -53,11 +53,9 @@ module cpu_ysyx_24100029
     wire                         inv_flag                   ;
     wire                         branch_flag                ;
 
-    wire               [  31: 0] csr_value                  ;
     wire               [  31: 0] mepc_out                   ;
-    wire               [  31: 0] mcause_out                 ;
-    wire               [  31: 0] mstatus_out                ;
     wire               [  31: 0] mtvec_out                  ;
+    wire               [  31: 0] csr_value                  ;
 
     wire               [  31: 0] mepc_in                    ;
     wire               [  31: 0] mcause_in                  ;
@@ -74,10 +72,7 @@ module cpu_ysyx_24100029
     assign                       dnpc                      = (jump_flag == 1'd1)? EX_result: (branch_flag == 1'b1 && EX_result != 32'd0)?  pc+({{20{imm[11]}},imm[11:0]}<<1) :pc+4;
     assign                       mem_wdata                 = rs2_value;
     assign                       Data_mem_valid            = mem_ren|mem_wen;
-    assign                       csr_value                 = (csr_wen[0])? mepc_out   :
-                                                             (csr_wen[1])? mcause_out :
-                                                             (csr_wen[2])? mstatus_out:
-                                                             (csr_wen[3])? mtvec_out:32'd0;
+
     assign                       mcause_in                 = (ecall_flag)? 32'd11:EX_result;// 11 means trigger environment from machine
     assign                       mepc_in                   = (ecall_flag)? snpc:EX_result;// save the trigger pc
     assign                       mstatus_in                = EX_result;
@@ -119,50 +114,6 @@ endtask
 
     export "DPI-C" task GetInst;
 
-Reg #(
-    .WIDTH                       (32                        ),
-    .RESET_VAL                   (0                         ) 
-) CSR_MEPC(
-    .clk                         (clk                       ),
-    .rst                         (rst_n                     ),
-    .din                         (mepc_in                   ),
-    .dout                        (mepc_out                  ),
-    .wen                         (csr_wen[0]                ) 
-);
-
-Reg #(
-    .WIDTH                       (32                        ),
-    .RESET_VAL                   (0                         ) 
-) CSR_MCAUSE(
-    .clk                         (clk                       ),
-    .rst                         (rst_n                     ),
-    .din                         (mcause_in                 ),
-    .dout                        (mcause_out                ),
-    .wen                         (csr_wen[1]                ) 
-);
-
-Reg #(
-    .WIDTH                       (32                        ),
-    .RESET_VAL                   (32'h1800                  ) 
-) CSR_MSTATUS(
-    .clk                         (clk                       ),
-    .rst                         (rst_n                     ),
-    .din                         (mstatus_in                ),
-    .dout                        (mstatus_out               ),
-    .wen                         (csr_wen[2]                ) 
-);
-
-Reg #(
-    .WIDTH                       (32                        ),
-    .RESET_VAL                   (0                         ) 
-) CSR_MTVEC(
-    .clk                         (clk                       ),
-    .rst                         (rst_n                     ),
-    .din                         (mtvec_in                  ),
-    .dout                        (mtvec_out                 ),
-    .wen                         (csr_wen[3]                ) 
-);
-
 
 
 IFU IFU_inst0(
@@ -195,17 +146,27 @@ EXU EXU_inst0
 
 
 Reg_Stack Reg_Stack_inst0(
-    .rst                         (rst_n                     ),
+    .rst_n                       (rst_n                     ),
     .clk                         (clk                       ),
     .rs1                         (rs1                       ),
     .rs2                         (rs2                       ),
     .rd                          (rd                        ),
     .rd_value                    (rd_value                  ),
+
     .R_wen                       (R_wen                     ),
+    .csr_wen                     (csr_wen                   ),
+
+    .mepc_in                     (mepc_in                   ),
+    .mcause_in                   (mcause_in                 ),
+    .mstatus_in                  (mstatus_in                ),
+    .mtvec_in                    (mtvec_in                  ),
 
     .rs1_value                   (rs1_value                 ),
     .rs2_value                   (rs2_value                 ),
-    .a0_value                    (a0_value                  ) 
+    .a0_value                    (a0_value                  ),
+    .csr_value                   (csr_value                 ),
+    .mepc_out                    (mepc_out                  ),
+    .mtvec_out                   (mtvec_out                 ) 
 );
 
 
