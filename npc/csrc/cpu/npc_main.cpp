2de41ba74adecdd59d0af225f640090071707c6a
tracer-ysyx <tracer@ysyx.org>
2024-12-24 22:44:34 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:44:34 up  4:54,  1 user,  load average: 0.81, 0.38, 0.22

--- npc/csrc/cpu/npc_main.cpp
+++ npc/csrc/cpu/npc_main.cpp
@@ -55,8 +55,8 @@ int main(int argc,char* argv[])
     init_monitor(argc, argv);
     
     // 将 m_trace 与 top 进行关联，其中5表示波形的采样深度为5级以下
- //   top->trace(m_trace, 5);
- //   m_trace->open("waveform.vcd");
+   top->trace(m_trace, 5);
+   m_trace->open("waveform.vcd");
 
     cpu_reset();
 
