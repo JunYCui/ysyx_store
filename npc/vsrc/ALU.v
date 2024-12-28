ada25bb38d9ecb7a6d3cece606feaa73393ca236
tracer-ysyx <tracer@ysyx.org>
2024-12-28 21:44:02 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:44:02 up  7:23,  1 user,  load average: 0.24, 0.50, 0.44

--- npc/vsrc/ALU.v
+++ npc/vsrc/ALU.v
@@ -87,7 +87,7 @@ always@(*)
     `alu_sll_ysyx_24100029:begin                                    //逻辑左移
             overflow = 1'b0;
             choose_add_sub = 1'b0;
-            res = d1<<d2;
+            res = d1<<d2[4:0];
     end
     `alu_srl_ysyx_24100029:begin                                    //逻辑右移
             overflow = 1'b0;
