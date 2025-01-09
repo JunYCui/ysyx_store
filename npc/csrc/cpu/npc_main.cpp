f961b2bf78ee71f41bae2de9cb55f1bbb093f5cc
tracer-ysyx <tracer@ysyx.org>
2025-01-09 10:47:18 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:47:18 up  1:58,  1 user,  load average: 0.62, 0.30, 0.36

--- npc/csrc/cpu/npc_main.cpp
+++ npc/csrc/cpu/npc_main.cpp
@@ -5,7 +5,7 @@
 #include "npc_sdb.h"
 extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
-    printf("addr = %d \n",addr);
+    printf("addr = %x \n",addr);
     *data = 1048691;   
     return ;
 }
