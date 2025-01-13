1e3d244c4e968f9f4588d2a467ecb04f94ad595e
tracer-ysyx <tracer@ysyx.org>
2025-01-13 16:16:32 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:32 up  1:33,  1 user,  load average: 0.51, 0.41, 0.48

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -2,36 +2,33 @@
 #include <klib-macros.h>
 #include <riscv/riscv.h>
 
-#define DEVICE_BASE 0xa0000000
-
-#define UART_ADDR (DEVICE_BASE + 0x00003f8)
-#define RTC_ADDR  (DEVICE_BASE + 0x0000048)
-
-
-extern char _heap_start;
-int main(const char *args);
+#define PMEM_SIZE (128 * 1024 * 1024)
+#define SERIAL_PORT 0x10000000
 
 extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
+extern char _heap_start;
+int main(const char *args);
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
+
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-  outb(UART_ADDR,ch);
+    outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
-  asm volatile("mv a0, %0; ebreak" : :"r"(code));
-  while (1) ;
-  
+    asm volatile("mv a0, %0; ebreak" : :"r"(code));
+
+  // should not reach here
+    while (1);
 }
 
 void _trm_init() {
-  int ret = main(mainargs);
-  halt(ret);
+    int ret = main(mainargs);
+    halt(ret);
 }
