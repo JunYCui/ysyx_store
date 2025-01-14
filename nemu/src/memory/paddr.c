1b216534d0e578e9e8d84dc5a2709afa339a82f1
tracer-ysyx <tracer@ysyx.org>
2025-01-14 14:40:07 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:07 up  3:11,  1 user,  load average: 0.61, 0.68, 0.51

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -19,13 +19,27 @@
 #include <isa.h>
 
 #if   defined(CONFIG_PMEM_MALLOC)
-static uint8_t *pmem = NULL;
+static uint8_t *rom = NULL;
 #else // CONFIG_PMEM_GARRAY
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
+static uint8_t *sram = NULL;
 
-uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
-paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+uint8_t* guest_to_host(paddr_t paddr) { return rom + paddr - CONFIG_ROM_SIZE; }
+paddr_t host_to_guest(uint8_t *haddr) { return haddr - rom + CONFIG_ROM_SIZE; }
+uint8_t* SRAM_guest_to_host(paddr_t paddr) { return sram + paddr - CONFIG_SRAM_SIZE; }
+
+static word_t sram_read(paddr_t addr, int len) {
+  word_t ret = host_read(SRAM_guest_to_host(addr), len);
+
+  IFDEF(CONFIG_MTRACE,printf("The address " ANSI_FMT(FMT_WORD, ANSI_FG_BLUE)  " is read at pc = " FMT_WORD "\n",addr,cpu.pc));
+  return ret;
+}
+
+static void sram_write(paddr_t addr, int len, word_t data) {
+  host_write(SRAM_guest_to_host(addr), len, data);
+  IFDEF(CONFIG_MTRACE,printf("The address " ANSI_FMT(FMT_WORD, ANSI_FG_BLUE) " is written at pc = " FMT_WORD "\n",addr,cpu.pc));
+}
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
@@ -33,35 +47,38 @@ static word_t pmem_read(paddr_t addr, int len) {
   IFDEF(CONFIG_MTRACE,printf("The address " ANSI_FMT(FMT_WORD, ANSI_FG_BLUE)  " is read at pc = " FMT_WORD "\n",addr,cpu.pc));
   return ret;
 }
-
+/*
 static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
   IFDEF(CONFIG_MTRACE,printf("The address " ANSI_FMT(FMT_WORD, ANSI_FG_BLUE) " is written at pc = " FMT_WORD "\n",addr,cpu.pc));
 }
-
+*/
 static void out_of_bound(paddr_t addr) {
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
-      addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+      addr, ROM_LEFT, ROM_RIGHT, cpu.pc);
 }
 
 void init_mem() {
 #if   defined(CONFIG_PMEM_MALLOC)
-  pmem = (uint8_t*)malloc(CONFIG_MSIZE);
-  assert(pmem);
+  rom = (uint8_t*)malloc(CONFIG_ROM_SIZE);
+  sram = (uint8_t*)malloc(CONFIG_SRAM_SIZE);
+  assert(rom);
 #endif
-  IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
-  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
+  IFDEF(CONFIG_MEM_RANDOM, memset(rom, rand(), CONFIG_ROM_SIZE));
+  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", ROM_LEFT, ROM_RIGHT);
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_rom(addr))) return pmem_read(addr, len);
+  else if(likely(in_sram(addr))) return sram_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_rom(addr))) { printf("rom can't write! \n");assert(0); return; }
+  else if(likely(in_sram(addr))) return sram_write(addr,len,data);
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
