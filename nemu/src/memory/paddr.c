3e15e4e68eade906f5ff148aa2336e9480c0fac4
tracer-ysyx <tracer@ysyx.org>
2025-01-14 14:51:52 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:52 up  3:23,  1 user,  load average: 0.58, 0.42, 0.42

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -77,8 +77,8 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_rom(addr))) { rom_write(addr,len,data); }
-  else if(likely(in_sram(addr))){sram_write(addr,len,data);}
+  if (likely(in_rom(addr))) { rom_write(addr,len,data); return; }
+  else if(likely(in_sram(addr))){sram_write(addr,len,data); return;}
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
