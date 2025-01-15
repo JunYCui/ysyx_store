d6a9e55e8888b1d74227d800a4caf340929cf408
tracer-ysyx <tracer@ysyx.org>
2025-01-15 15:52:05 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:52:05 up  3:44,  1 user,  load average: 0.18, 0.21, 0.32

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,16 +1,15 @@
 #include <am.h>
 #include <klib-macros.h>
 #include <riscv/riscv.h>
-
-#define PMEM_SIZE (128 * 1024 * 1024)
+#include <klib.h>
+#define SRAM_SIZE (8 * 1024)
 #define SERIAL_PORT 0x10000000
 
-extern char _pmem_start;
-extern char _heap_start;
+extern char data_load_start[];
+extern char data_size[];
+extern char _heap_start[];
 int main(const char *args);
-#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
-
-Area heap = RANGE(&_heap_start, PMEM_END);
+Area heap = RANGE(_heap_start, _heap_start+SRAM_SIZE);
 
 #ifndef MAINARGS
 #define MAINARGS ""
@@ -29,6 +28,7 @@ void halt(int code) {
 }
 
 void _trm_init() {
+    memcpy(_heap_start,data_load_start,(size_t)data_size);
     int ret = main(mainargs);
     halt(ret);
 }
