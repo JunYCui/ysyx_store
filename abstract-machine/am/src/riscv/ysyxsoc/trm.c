6a73ce389aa00e63da93fe527be44c2b9f50718c
tracer-ysyx <tracer@ysyx.org>
2025-01-17 15:18:56 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:18:56 up  2:24,  1 user,  load average: 0.38, 0.40, 0.41

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -25,7 +25,7 @@ Area heap = RANGE(_heap_start, _heap_start+SRAM_SIZE);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-    char status = inb(LSR)&0x20; // 6th Bits of LSR 
+    char status = inb(LSR)&0x01; // 6th Bits of LSR 
     while (status!=0)
     {
         status = inb(LSR);
@@ -35,12 +35,12 @@ void putch(char ch) {
 }
 
 void uart_init(){
-    //outb(LCR,0x83); // Set 8 bits of each character and allow access to the Devisor Latch
-    //utb(Divisor_MSB,0x00);
-    //outb(Divisor_LSB,0x00);
+    outb(LCR,0x83); // Set 8 bits of each character and allow access to the Devisor Latch
+    outb(Divisor_MSB,0x00);
+    outb(Divisor_LSB,0x01);
     outb(LCR,0x03); // clear access to the Driver Latch
     outb(FCR,0xc0); // set fifo interupt triggle level = 14bytes
-    //outb(IER,0x00); // disable all interrupt 
+    outb(IER,0x00); // disable all interrupt 
 }
 void halt(int code) {
     asm volatile("mv a0, %0; ebreak" : :"r"(code));
