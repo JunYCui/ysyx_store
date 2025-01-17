1bf382d0d50120f7c6c0d3fb93bd018dbf371ae6
tracer-ysyx <tracer@ysyx.org>
2025-01-17 14:42:48 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:42:48 up  1:47,  1 user,  load average: 0.29, 0.34, 0.30

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -5,6 +5,12 @@
 #define SRAM_SIZE (8 * 1024)
 #define SERIAL_PORT 0x10000000
 
+#define LCR (SERIAL_PORT+0x03)
+#define Divisor_LSB  (SERIAL_PORT+0x00)
+#define Divisor_MSB  (SERIAL_PORT+0x01)
+#define FCR (SERIAL_PORT+0x02)
+#define IER (SERIAL_PORT+0x01)
+
 extern char data_load_start[];
 extern char data_size[];
 extern char _heap_start[];
@@ -21,6 +27,14 @@ void putch(char ch) {
     outb(SERIAL_PORT, ch);
 }
 
+void uart_init(){
+    outb(LCR,0x83); // Set 8 bits of each character and allow access to the Devisor Latch
+    outb(Divisor_MSB,0x01);
+    outb(Divisor_LSB,0x01);
+    outb(LCR,0x03); // clear access to the Driver Latch
+    outb(FCR,0xc0); // set fifo interupt triggle level = 14bits
+    outb(IER,0x01); // disable all interrupt 
+}
 void halt(int code) {
     asm volatile("mv a0, %0; ebreak" : :"r"(code));
 
@@ -30,6 +44,7 @@ void halt(int code) {
 
 void _trm_init() {
     memcpy(data_start,data_load_start,(size_t)data_size);
+    uart_init();
     int ret = main(mainargs);
     halt(ret);
 }
