951fb3dbc406caf50479007854f6c9f486821645
JunYCui <1354315077@qq.com>
2025-01-17 09:41:18 +0800

    mem_test finishes

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -11,8 +11,8 @@ AM_SRCS := platform/nemu/trm.c \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
-             --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
-LDFLAGS   += --gc-sections -e _start
+             --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x60000000
+LDFLAGS   += --gc-sections -e _start --print-map
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt  -e $(IMAGE).elf -b
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
--- npc/vsrc/CPU/Main_Module/ysyx_24100029_LSU.v
+++ npc/vsrc/CPU/Main_Module/ysyx_24100029_LSU.v
@@ -88,7 +88,7 @@ module ysyx_24100029_LSU (
     wire               [  31: 0] rdata_16u                  ;
 
     wire               [  31: 0] rdata_ex                   ;
-
+    wire               [   4: 0] rdata_b_choice             ;
 
     reg                [  31: 0] pc_reg                     ;
     reg                          mem_ren_reg                ;
@@ -213,18 +213,19 @@ end
     
 
     assign                       awaddr                    = Ex_result_reg;
-    assign                       awid                      = 0;
+    assign                       awid                      = 1;
     assign                       awlen                     = 0;
-    assign                       awsize                    = 3'b010;
+    assign                       awsize                    = 2;
     assign                       awburst                   = 0;
 
-    assign                       wdata                     = rs2_value_reg;
+    assign                       wdata                     = rs2_value_reg<<8*Ex_result_reg[1:0];
+    assign                       rdata_b_choice            = {araddr[1:0],3'b0};
+    
+    assign wstrb = (funct3_reg == 3'b000)                           ?
+                    4'b0001<<Ex_result_reg[1:0]:(funct3_reg == 3'b001)     ?
+                    4'b0011<<Ex_result_reg[1:0]:(funct3_reg == 3'b010)     ?
+                    4'b1111:4'b0000;
 
-ysyx_24100029_MuxKeyInternal #(i5_NR_KEY, i5_KEY_LEN, i5_DATA_LEN) i5 (wstrb, funct3_reg, {i5_DATA_LEN{1'b0}},{
-  3'b000,4'b0001,
-  3'b001,4'b0011,
-  3'b010,4'b1111
-});
 
     assign                       arid                      = 0;
     assign                       arlen                     = 0;
@@ -302,8 +303,8 @@ ysyx_24100029_MuxKeyInternal #(i4_NR_KEY, i4_KEY_LEN, i4_DATA_LEN) i4 (rdata_ex,
   3'b101,rdata_16u                                                  // 1hu
 });
 
-    assign                       rdata_8u                  = {24'd0,rdata[7:0]};
-    assign                       rdata_16u                 = {16'd0,rdata[15:0]};
+    assign                       rdata_8u                  = {24'd0,rdata[rdata_b_choice +: 8]};
+    assign                       rdata_16u                 = {16'd0,rdata[rdata_b_choice +: 16]};
 
 /* verilator lint_off PINMISSING */
 ysyx_24100029_sext #(
@@ -311,7 +312,7 @@ ysyx_24100029_sext #(
     .OUT_WIDTH                   (32                        ) 
 ) sext_i8
 (
-    .data                        (rdata[7:0]                ),
+    .data                        (rdata[rdata_b_choice +:8] ),
     .sext_data                   (rdata_8i                  ) 
 );
 
@@ -320,7 +321,7 @@ ysyx_24100029_sext #(
     .OUT_WIDTH                   (32                        ) 
 ) sext_i16
 (
-    .data                        (rdata[15:0]               ),
+    .data                        (rdata[rdata_b_choice+:16] ),
     .sext_data                   (rdata_16i                 ) 
 );
 
