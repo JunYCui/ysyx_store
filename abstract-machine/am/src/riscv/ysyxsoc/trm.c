e7214cae88eab2e6738115025f5ae6392adf3772
tracer-ysyx <tracer@ysyx.org>
2025-01-17 16:02:48 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:02:48 up  3:07,  1 user,  load average: 0.81, 0.43, 0.43

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -25,10 +25,7 @@ Area heap = RANGE(_heap_start, _heap_start+SRAM_SIZE);
 static const char mainargs[] = MAINARGS;
 
 void uart_init(){
-    char r;
-    outb(LCR,0x83); // Set 8 bits of each character and allow access to the Devisor Latch
-    r = inb(LCR);
-    outb(LCR,r); 
+    outb(LCR,0x83); // Set 8 bits of each character and allow access to the Devisor Latch 
     outb(Divisor_MSB,0x00);
     outb(Divisor_LSB,0x01);
     outb(LCR,0x03); // clear access to the Driver Latch
