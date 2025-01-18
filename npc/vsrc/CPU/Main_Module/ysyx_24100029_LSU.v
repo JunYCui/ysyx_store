1aba91f019d39aab8d09b6a715e94b9d439974c1
tracer-ysyx <tracer@ysyx.org>
2025-01-18 09:53:26 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:53:26 up  2:15,  1 user,  load average: 0.34, 0.37, 0.35

--- npc/vsrc/CPU/Main_Module/ysyx_24100029_LSU.v
+++ npc/vsrc/CPU/Main_Module/ysyx_24100029_LSU.v
@@ -230,10 +230,7 @@ end
     assign                       arid                      = 0;
     assign                       arlen                     = 0;
 
-    assign arsize = (funct3_reg == 3'b000 | funct3 == 3'b100)                           ?
-                    3'b000:(funct3_reg == 3'b001 | funct3_reg == 3'b101)     ?
-                    3'b001:(funct3_reg == 3'b010)     ?
-                    3'b010:3'b0000;
+    assign arsize = 3'b101;
 
     assign                       arburst                   = 0;
 
