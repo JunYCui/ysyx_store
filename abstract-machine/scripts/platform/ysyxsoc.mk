fc8f89c1a90f88d0bf1d2d95ebeafca9d88f394e
tracer-ysyx <tracer@ysyx.org>
2025-01-23 18:53:38 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:53:38 up  3:12,  1 user,  load average: 0.89, 0.49, 0.31

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -5,7 +5,7 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/ysyxsoclinker.ld
 LDFLAGS   += --gc-sections -e _start --print-map
 CFLAGS += -DMAINARGS=\"$(mainargs)\" 
-SOCFLAGS = -e $(IMAGE).elf -b
+SOCFLAGS = -e $(IMAGE).elf 
 
 
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
