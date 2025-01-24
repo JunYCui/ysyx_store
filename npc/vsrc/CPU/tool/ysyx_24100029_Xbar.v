370356f3155f89a088b73975c08bbcfc58fa79e8
tracer-ysyx <tracer@ysyx.org>
2025-01-24 16:38:23 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:38:23 up  3:27,  1 user,  load average: 1.48, 0.93, 0.66

--- npc/vsrc/CPU/tool/ysyx_24100029_Xbar.v
+++ npc/vsrc/CPU/tool/ysyx_24100029_Xbar.v
@@ -132,6 +132,7 @@ module ysyx_24100029_Xbar(
     input              [   3: 0]           SOC_rid                     
 );
     initial begin
+            if(araddr[31:24] == 8'h02)
         $monitor("araddr = %d \n",araddr);
     end
 
