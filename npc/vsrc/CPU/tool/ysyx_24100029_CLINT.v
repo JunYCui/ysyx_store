625efd334742815bec4de5c2b5f9d6345d4dbbe5
tracer-ysyx <tracer@ysyx.org>
2025-01-24 18:45:27 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:45:27 up  4:00,  1 user,  load average: 0.79, 0.75, 0.74

--- npc/vsrc/CPU/tool/ysyx_24100029_CLINT.v
+++ npc/vsrc/CPU/tool/ysyx_24100029_CLINT.v
@@ -69,7 +69,7 @@ module ysyx_24100029_CLINT
     reg                [  31: 0]        clk_count_high              ;
 
     initial begin
-        $monitor($time,"araddr = %d \n",araddr);
+        $monitor($time,,"araddr = %d \n",araddr);
     end
 
     always @(posedge clock) begin
