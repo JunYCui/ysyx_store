307bade44edfb626bd5f927f991315192bceba2b
tracer-ysyx <tracer@ysyx.org>
2025-01-30 16:45:36 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:36 up  6:38,  1 user,  load average: 0.80, 0.50, 0.38

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -12,6 +12,9 @@
 #define IER (SERIAL_PORT+0x01)
 #define LSR (SERIAL_PORT+0x05)
 
+extern char _ssbl_size[] ;
+extern char _ssbl_start[];
+extern char _ssbl_loadstart[];
 extern char data_load_start[];
 extern char data_size[];
 extern char _heap_start[];
@@ -59,9 +62,30 @@ void halt(int code) {
   // should not reach here
     while (1);
 }
+
+void __attribute__((section(".fsbl"))) fsbl_init(){
+    char *p0=_ssbl_start;
+    const char*p1 = _ssbl_loadstart;
+    size_t n = (size_t) _ssbl_size;
+    while(n--)
+    {
+      *(p0++) = *(p1++);
+    }
+}
+
+void __attribute__((section(".ssbl"))) ssbl_init(){
+    char *p0=data_start;
+    const char*p1 = data_load_start;
+    size_t n = (size_t) data_size;
+    while(n--)
+    {
+      *(p0++) = *(p1++);
+    }
+    return ;
+} 
+
 void _trm_init() {
     uart_init();
-    memcpy(data_start, data_load_start,(size_t)data_size);
     id_display();
     int ret = main(mainargs);
     halt(ret);
