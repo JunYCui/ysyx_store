30feec2aaed550d17e57b7aa4835db2f45c29703
tracer-ysyx <tracer@ysyx.org>
2025-02-05 00:51:21 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:51:21 up 13:50,  1 user,  load average: 0.20, 0.21, 0.27

--- ysyxSoC/perip/sdram/sdram.v
+++ ysyxSoC/perip/sdram/sdram.v
@@ -138,6 +138,8 @@ module sdram(
     always @(posedge clk) begin
         if(!cke)
           col_addr <= 0;
+        else if(state == idle)
+          col_addr <= 0;
         else if(read_cmd & ba == bank_addr)
           col_addr <= {a[11],a[9:0]};
         else if(write_cmd & ba == bank_addr)
