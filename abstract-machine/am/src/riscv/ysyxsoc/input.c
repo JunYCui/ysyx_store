7f32af1d47365cbb7bff2f157aaabc56abb5654f
tracer-ysyx <tracer@ysyx.org>
2025-02-20 23:46:42 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:46:42 up 1 day,  6:42,  1 user,  load average: 4.80, 4.54, 3.41

--- abstract-machine/am/src/riscv/ysyxsoc/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/input.c
@@ -9,7 +9,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   int i;
   if(code == 0xf0){
     code = inb(KBR_ADDR);
-    kbd->keydown = true;
+    kbd->keydown = false;
     kbd->keycode = code -1;
     for(i=0;i<count;i++){
       if(code == reg[i])
@@ -18,17 +18,21 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
     for(int j=i+1;j<count;j++){
         reg[j-1] = reg[j];
     }
+    count --;
   }
-  else {
+  else if(code !=0){
     for(i=0;i<count;i++){
         if(code == reg[i])
           break;
     }
     if(i == count){
-      reg[count++] = code-1;
-      kbd->keydown = false;
+      reg[count++] = code;
+      kbd->keycode = code -1;
+      kbd->keydown = true;
     }
     else 
       kbd->keycode = AM_KEY_NONE;
   }
+  else 
+    kbd->keycode = AM_KEY_NONE;
 }
