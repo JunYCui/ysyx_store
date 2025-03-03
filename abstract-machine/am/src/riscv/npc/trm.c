7a6f751f4803547f59ebe7ad4faa12acab97eece
tracer-ysyx <tracer@ysyx.org>
2025-03-03 15:17:17 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:17:17 up 55 min,  1 user,  load average: 0.68, 0.58, 0.55

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -13,7 +13,7 @@ int main(const char *args);
 
 extern char _stack_top;
 #define PMEM_SIZE (1 * 1024 * 1024)
-#define PMEM_END  ((uintptr_t)&_stack_top - PMEM_SIZE)
+#define PMEM_END  ((uintptr_t)&_stack_top + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
