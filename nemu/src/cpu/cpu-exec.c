b9e42af4380e0d40754b1d6119534b1723313bbb
tracer-ysyx <tracer@ysyx.org>
2025-03-04 15:41:12 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:12 up  6:25,  1 user,  load average: 0.21, 0.31, 0.35

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -73,7 +73,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
   static uint64_t count=0;
   static bool start = 1;
   static bool jump_flag;
-  char *fp = strtok(_this->logbuf,"\t");
+  char *fp = strtok(_this->logbuf,":");
   printf("%s\n",fp);
   fp = strtok(_this->logbuf,"\t");
 #ifdef CONFIG_ITRACE_COND
