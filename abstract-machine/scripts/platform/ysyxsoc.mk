f03f3519fc83019dbcc78621e41098792b730f21
tracer-ysyx <tracer@ysyx.org>
2025-03-04 10:41:31 +0800

    > compile NEMU ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:41:31 up  1:25,  1 user,  load average: 0.58, 0.67, 0.45

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -16,7 +16,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/ysyxsoclinker.ld  #--print-map
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\" 
 SOCFLAGS = -e $(IMAGE).elf -b
-
+NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt  -e $(IMAGE).elf -b
 
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
 
@@ -32,4 +32,4 @@ gdb: image
 	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) gdb ARGS="$(SOCFLAGS)" IMG=$(IMAGE).bin
 
 cache: image
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(SOCFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
