462727f17ff15614e1f174455ce1536b6a8928ea
tracer-ysyx <tracer@ysyx.org>
2025-03-27 22:29:37 +0800

    > sim RTL ysyx_24100029 Cui Junyan Linux cjy-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:29:37 up 13:38,  1 user,  load average: 0.61, 0.61, 0.54

--- npc/vsrc/CPU/Main_Module/ysyx_24100029_Control.v
+++ npc/vsrc/CPU/Main_Module/ysyx_24100029_Control.v
@@ -45,9 +45,16 @@ module ysyx_24100029_Control (
     wire               [   1: 0]        IDU_rs1_choice              ;
     wire               [   1: 0]        IDU_rs2_choice              ;
 
-    assign                              dnpc_flag                   = ( EXU_valid& (jump_flag|fence_i_flag|(branch_flag&Ex_result[0]))) || (IDU_valid & (mret_flag|ecall_flag));
+   // assign                              dnpc_flag                   = ( EXU_valid& (jump_flag|fence_i_flag|(branch_flag&Ex_result[0]))) || (IDU_valid & (mret_flag|ecall_flag));
 
-    assign                              IDU_inst_clear              = (EXU_valid& (jump_flag|fence_i_flag|(branch_flag&Ex_result[0]))) || (IDU_valid & (mret_flag|ecall_flag));
+//    assign                              IDU_inst_clear              = (EXU_valid& (jump_flag|fence_i_flag|(branch_flag&Ex_result[0]))) || (IDU_valid & (mret_flag|ecall_flag));
+   // assign                              icache_clr                  = fence_i_flag&EXU_valid;
+
+
+
+    assign                              dnpc_flag                   = ((mret_flag&IDU_valid) || (ecall_flag&IDU_valid) || (jump_flag&EXU_valid) || (branch_flag&EXU_valid&Ex_result[0] != 0) || (fence_i_flag&EXU_valid) );
+
+    assign                              IDU_inst_clear              = ((mret_flag&IDU_valid) || (ecall_flag&IDU_valid) || (jump_flag&EXU_valid) || (branch_flag&EXU_valid&Ex_result[0] != 0) || (fence_i_flag&EXU_valid) );
     assign                              icache_clr                  = fence_i_flag&EXU_valid;
 
 
