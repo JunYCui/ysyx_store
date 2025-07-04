151605d3c3f3afd12f720bb275719acc1ce2eb23
JunYCui <1354315077@qq.com>
2025-07-04 13:45:02 +0800

    debug

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -11,7 +11,7 @@ AM_SRCS := riscv/npc/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
-						 --defsym=_pmem_start=0x00000000 --defsym=_entry_offset=0x0
+						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\" 
 NPCFLAGS = -e $(IMAGE).elf -b
